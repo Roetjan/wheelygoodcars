fb6d494d4fe7ea926d5c6989d344c078e9dc06a4
Roetjan <93587343+Roetjan@users.noreply.github.com>
2023-03-31 11:19:09 +0200

    license plate validation

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -16,7 +16,9 @@ class CarController extends Controller
     }
 
     public function create_step2(Request $request){
-        $api = Http::get('https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken='.$request->kenteken.'&$limit=1000&$$app_token=84A6fXMy11X3HoLa8GVLKhahJ');
+        $kenteken = str_replace('-','',$request->kenteken);
+        $kenteken = strtoupper($kenteken);
+        $api = Http::get('https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken='. $kenteken .'&$limit=1000&$$app_token=84A6fXMy11X3HoLa8GVLKhahJ');
         $jsonData = json_decode($api);
         if($jsonData == null) {
             return redirect()->route('cars.create')->withErrors(['kenteken' => 'Kenteken niet gevonden']);
