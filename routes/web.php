1180efcba77c325d6b360b1360bc2d2e5ec2dd08
Roetjan <93587343+Roetjan@users.noreply.github.com>
2023-04-14 10:59:54 +0200

    added routes for edit form

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,9 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/cars/create', [CarController::class, 'create_step1'])->name('cars.create1');
+    Route::post('/cars/create_form', [CarController::class, 'create_step2'])->name('cars.create2');
+    Route::post('/cars/create', [CarController::class, 'create_car'])->name('cars.create');
 
+    Route::get('/cars/edit_form/{id}', [CarController::class, 'edit'])->name('cars.edit');
 });
-
-Route::post('/cars/create_form', [CarController::class, 'create_step2'])->name('cars.create2');
-Route::post('/cars/create', [CarController::class, 'create_car'])->name('cars.create');
-
-
 require __DIR__.'/auth.php';
